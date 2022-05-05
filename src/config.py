975f481a41aed7bfc1cf9e27cf9e5c88cdec6be6
evallina <eloyvallina33@gmail.com>
2022-05-05 17:09:15 +0200

    More paths and some comments

--- src/config.py
+++ src/config.py
@@ -1,9 +1,24 @@
 from pathlib import Path
 
 ROOT = Path(__file__).parent.parent
-QUERY_DIR = ROOT / 'searches/queries'
+
+
+# sequence databases
 DATA_DIR = ROOT / 'data'
 UP_DIR = DATA_DIR / 'uniprot'
 NCBI_DIR = DATA_DIR / 'ncbi'
+
+# search queries and results
 SEARCH_DIR = ROOT / "searches"
+QUERY_DIR = SEARCH_DIR / 'queries'
 HALO_DIR = SEARCH_DIR / "halocyanin"
+CYTO_DIR = SEARCH_DIR / "cytochrome"
+
+# target fasta files
+TARGETS = ROOT / "targets"
+
+# average proteome data
+PROTEOMEDATA = ROOT / "analysis" / "proteomedata"
+
+# prosite results
+PROSITES = ROOT / "prosites"
